88d06d9169775ae21676d3c461c904daef9be25d
Jessica Loo <jessicaloohw@gmail.com>
2018-03-20 13:52:19 -0400

    add checks to validate inputs

--- my_mongo_service.py
+++ my_mongo_service.py
@@ -1,62 +1,31 @@
 from flask import Flask, jsonify, request
 from main import add_heart_rate, create_user
-from pymodm import connect
+from pymodm import connect, errors
 from datetime import datetime
 import models
 
 app = Flask(__name__)
 
-@app.route("/api/heart_rate", methods=["POST"])
-def api_heart_rate_post():
-
-    connect("mongodb://vcm-3486.vm.duke.edu:27017/heart_rate_app")
-
-    input = request.get_json()
-    user_email = input["user_email"]
-    user_age = input["user_age"]
-    user_heart_rate = input["heart_rate"]
+def validate_input_post(input):
 
     try:
-        user = models.User.objects.raw({"_id":user_email}).first()
-        add_heart_rate(email=user_email, heart_rate=user_heart_rate, time=datetime.now())
-        data = {"message": "Heart rate added to user."}
-        return jsonify(data)
-    except:
-        create_user(email=user_email, age=user_age, heart_rate=user_heart_rate)
-        data = {"message": "New user created."}
-        return jsonify(data)
+        user_email = input["user_email"]
+        user_age = input["user_age"]
+        user_heart_rate = input["heart_rate"]
+        return user_email, user_age, user_heart_rate
+    except KeyError:
+        return None, None, None
 
-@app.route("/api/heart_rate/<user_email>", methods=["GET"])
-def api_heart_rate_get(user_email):
-
-    connect("mongodb://vcm-3486.vm.duke.edu:27017/heart_rate_app")
-
+def validate_input_interval_average(input):
     try:
-        user = models.User.objects.raw({"_id":user_email}).first()
-        data = {"user_email": user.email,
-                "heart_rate": user.heart_rate}
-        return jsonify(data)
-    except:
-        data = {"message": "User not found."}
-        return jsonify(data)
-
-@app.route("/api/heart_rate/average/<user_email>", methods=["GET"])
-def api_heart_rate_average(user_email):
-
-    import numpy as np
-
-    connect("mongodb://vcm-3486.vm.duke.edu:27017/heart_rate_app")
-
-    try:
-        user = models.User.objects.raw({"_id":user_email}).first()
-        heart_rate = user.heart_rate
-        average_heart_rate = np.mean(heart_rate)
-        data = {"user_email": user.email,
-                "average_heart_rate": average_heart_rate}
-        return jsonify(data)
-    except:
-        data = {"message": "User not found."}
-        return jsonify(data)
+        user_email = input["user_email"]
+        time_string = input["heart_rate_average_since"]
+        time_formatted = datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S.%f")
+        return user_email, time_string, time_formatted
+    except KeyError:
+        return None, None, None
+    except ValueError:
+        return None, None, None
 
 def check_tachycardia(user_age, user_average_heart_rate):
 
@@ -91,6 +60,59 @@ def check_tachycardia(user_age, user_average_heart_rate):
         else:
             return False
 
+@app.route("/api/heart_rate", methods=["POST"])
+def api_heart_rate_post():
+
+    connect("mongodb://vcm-3486.vm.duke.edu:27017/heart_rate_app")
+
+    input = request.get_json()
+    user_email, user_age, user_heart_rate = validate_input_post(input)
+    if(user_email is None):
+        data = {"message": "Wrong inputs."}
+        return jsonify(data)
+
+    try:
+        user = models.User.objects.raw({"_id":user_email}).first()
+        add_heart_rate(email=user_email, heart_rate=user_heart_rate, time=datetime.now())
+        data = {"message": "Heart rate added to user."}
+        return jsonify(data)
+    except errors.DoesNotExist:
+        create_user(email=user_email, age=user_age, heart_rate=user_heart_rate)
+        data = {"message": "New user created."}
+        return jsonify(data)
+
+@app.route("/api/heart_rate/<user_email>", methods=["GET"])
+def api_heart_rate_get(user_email):
+
+    connect("mongodb://vcm-3486.vm.duke.edu:27017/heart_rate_app")
+
+    try:
+        user = models.User.objects.raw({"_id":user_email}).first()
+        data = {"user_email": user.email,
+                "heart_rate": user.heart_rate}
+        return jsonify(data)
+    except errors.DoesNotExist:
+        data = {"message": "User not found."}
+        return jsonify(data)
+
+@app.route("/api/heart_rate/average/<user_email>", methods=["GET"])
+def api_heart_rate_average(user_email):
+
+    import numpy as np
+
+    connect("mongodb://vcm-3486.vm.duke.edu:27017/heart_rate_app")
+
+    try:
+        user = models.User.objects.raw({"_id":user_email}).first()
+        heart_rate = user.heart_rate
+        average_heart_rate = np.mean(heart_rate)
+        data = {"user_email": user.email,
+                "average_heart_rate": average_heart_rate}
+        return jsonify(data)
+    except errors.DoesNotExist:
+        data = {"message": "User not found."}
+        return jsonify(data)
+
 @app.route("/api/heart_rate/interval_average", methods=["POST"])
 def api_heart_rate_interval_average():
 
@@ -99,9 +121,10 @@ def api_heart_rate_interval_average():
     connect("mongodb://vcm-3486.vm.duke.edu:27017/heart_rate_app")
 
     input = request.get_json()
-    user_email = input["user_email"]
-    time_string = input["heart_rate_average_since"]
-    time_formatted = datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S.%f")
+    user_email, time_string, time_formatted = validate_input_interval_average(input)
+    if(user_email is None):
+        data = {"message": "Wrong inputs."}
+        return jsonify(data)
 
     try:
         user = models.User.objects.raw({"_id":user_email}).first()
@@ -122,6 +145,6 @@ def api_heart_rate_interval_average():
                 "interval_average": interval_average,
                 "tachycardia": tachycardia}
         return jsonify(data)
-    except:
+    except errors.DoesNotExist:
         data = {"message": "User not found."}
         return jsonify(data)
